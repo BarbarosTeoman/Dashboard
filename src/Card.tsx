31a8e670e6b157149499c349cc9bdc79db59ede2
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2023-06-24 22:06:31 +0300

    update

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,7 +3,7 @@ export default function Card(props: any) {
   return (
     <div className="card">
       <div className="sectionBackground">
-        <img src={`src/assets/icon-${props.title.toLowerCase().split(" ").join("-")}.svg`} alt="" />
+        <embed src={`src/assets/icon-${props.title.toLowerCase().split(" ").join("-")}.svg`}/>
       </div>
       <div className={"cardInformation"}>
         <div className="topOfCard">
