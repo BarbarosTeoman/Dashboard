19566d2284b661c2562ca5f7468c3c64f5e4b009
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2023-08-02 13:10:29 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,67 @@
-import './App.css'
-import data from './data.json'
-import Card from './Card'
-import { useState } from 'react'
+import "./App.css"
+import data from "./data.json"
+import Card from "./Card"
+import { useState } from "react"
 
 function App() {
+	const [timeFrame, setTimeFrame] = useState<string>("daily")
 
-  const [timeFrame, setTimeFrame] = useState<string>('daily')
+	let cardTimeFrame
 
-  let cardTimeFrame;
+	if (timeFrame === "daily") {
+		const timeText = "Yesterday"
+		cardTimeFrame = data.map((item: any) => (
+			<Card
+				title={item.title}
+				timeframe={timeText}
+				current={item.timeframes.daily.current}
+				previous={item.timeframes.daily.previous}
+			/>
+		))
+	} else if (timeFrame === "weekly") {
+		const timeText = "Last Week"
+		cardTimeFrame = data.map((item: any) => (
+			<Card
+				title={item.title}
+				timeframe={timeText}
+				current={item.timeframes.weekly.current}
+				previous={item.timeframes.weekly.previous}
+			/>
+		))
+	} else if (timeFrame === "monthly") {
+		const timeText = "Last Month"
+		cardTimeFrame = data.map((item: any) => (
+			<Card
+				title={item.title}
+				timeframe={timeText}
+				current={item.timeframes.monthly.current}
+				previous={item.timeframes.monthly.previous}
+			/>
+		))
+	}
 
-  if (timeFrame === "daily") {
-    const timeText = "Yesterday"
-    cardTimeFrame = data.map((item: any) =>
-      <Card title={item.title} timeframe={timeText} current={item.timeframes.daily.current} previous={item.timeframes.daily.previous}/>
-  )}
-  else if (timeFrame === "weekly") {
-    const timeText = "Last Week"
-    cardTimeFrame = data.map((item: any) =>
-      <Card title={item.title} timeframe={timeText} current={item.timeframes.weekly.current} previous={item.timeframes.weekly.previous}/>
-  )}
-  else if (timeFrame === "monthly") {
-    const timeText = "Last Month"
-    cardTimeFrame = data.map((item: any) =>
-      <Card title={item.title} timeframe={timeText} current={item.timeframes.monthly.current} previous={item.timeframes.monthly.previous}/>
-  )}
-
-  return (
-    <div className='main'>
-      <div className="container">
-        <div className="personCard">
-          <div className="person">
-            <img src="public/image-jeremy.png" alt="photo"/>
-            <div className="personInformation">
-              <p>Report for</p>
-              <h1>Jeremy</h1>
-              <h1>Robson</h1>
-            </div>
-          </div>
-          <div className="timeFrames">
-            <button onClick={_e => setTimeFrame("daily")}>Daily</button>
-            <button onClick={_e => setTimeFrame("weekly")}>Weekly</button>
-            <button onClick={_e => setTimeFrame("monthly")}>Monthly</button>
-          </div>
-        </div>
-        {cardTimeFrame}
-      </div>  
-    </div>
-  )
+	return (
+		<div className="main">
+			<div className="container">
+				<div className="personCard">
+					<div className="person">
+						<img src="/image-jeremy.png" alt="photo" />
+						<div className="personInformation">
+							<p>Report for</p>
+							<h1>Jeremy</h1>
+							<h1>Robson</h1>
+						</div>
+					</div>
+					<div className="timeFrames">
+						<button onClick={(_e) => setTimeFrame("daily")}>Daily</button>
+						<button onClick={(_e) => setTimeFrame("weekly")}>Weekly</button>
+						<button onClick={(_e) => setTimeFrame("monthly")}>Monthly</button>
+					</div>
+				</div>
+				{cardTimeFrame}
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,20 +1,29 @@
 export default function Card(props: any) {
-
-  return (
-    <div className="card">
-      <div className="sectionBackground">
-        <img src={`public/icon-${props.title.toLowerCase().split(" ").join("-")}.svg`}/>
-      </div>
-      <div className={"cardInformation"}>
-        <div className="topOfCard">
-          <h4>{props.title}</h4>
-          <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
-        </div>
-        <div className="bottomOfCard">
-          <h1>{props.current}hrs</h1>
-          <p>{props.timeframe} - {props.previous}hrs</p>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="card">
+			<div className="sectionBackground">
+				<img
+					src={`/icon-${props.title.toLowerCase().split(" ").join("-")}.svg`}
+				/>
+			</div>
+			<div className={"cardInformation"}>
+				<div className="topOfCard">
+					<h4>{props.title}</h4>
+					<svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+							fill="#BBC0FF"
+							fill-rule="evenodd"
+						/>
+					</svg>
+				</div>
+				<div className="bottomOfCard">
+					<h1>{props.current}hrs</h1>
+					<p>
+						{props.timeframe} - {props.previous}hrs
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
